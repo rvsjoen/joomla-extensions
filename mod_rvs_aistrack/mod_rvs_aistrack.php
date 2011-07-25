9676565b63fdd4f3c9633954408f0c9d9fe803e0
Rune V. Sjøen <rvsjoen@gmail.com>
2011-07-25 17:07:50 +0200

    Added AISTrack module

---
+++ mod_rvs_aistrack/mod_rvs_aistrack.php
@@ -0,0 +1,12 @@
+<?php defined('_JEXEC') or die;
+/**
+ * @package		RVS
+ * @subpackage	AISTrack
+ * @author		Rune V. Sjøen <rvsjoen@gmail.com>
+ * @copyright	Copyright (C) 2011 {@link http://www.twilight-zone.com}
+ * @license		http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+$doc = JFactory::getDocument();
+
+require JModuleHelper::getLayoutPath('mod_rvs_aistrack', $params->get('layout', 'default'));
